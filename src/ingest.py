757e08febe1bb732cab5a33ad43855c0e9b6de55
anjannis <an.jannis@icloud.com>
2026-09-22 23:13:30 +0200

    started the load_info() function

--- src/ingest.py
+++ src/ingest.py
@@ -4,3 +4,20 @@ from pathlib import Path
 RAW_DIR = Path(__file__).parent.parent / "data" / "raw"
 
 print(RAW_DIR)
+
+
+def load_info() -> pd.DataFrame:
+    """
+    Load the STRING protein info file into a DataFrame.
+    """
+    path = RAW_DIR / "9606.protein.info.v12.0.txt"
+
+    df = pd.read_csv(path, sep="\t", header=0)
+    return df
+
+
+if __name__ == "__main__":
+    df = load_info()
+    print(df.head())
+    print(df.shape)
+    print(df.dtypes)
