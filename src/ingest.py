2ce2a4aa500b0ec93aeae835d70359764ede1290
anjannis <an.jannis@icloud.com>
2026-09-22 23:55:35 +0200

    started the load_links() function

--- src/ingest.py
+++ src/ingest.py
@@ -15,6 +15,14 @@ def load_info() -> pd.DataFrame:
     df = pd.read_csv(path, sep="\t", header=0)
     return df
 
+def load_links() -> pd.DataFrame:
+    """
+    Load the STRING protein link file into a Dataframe.
+    """
+    path = RAW_DIR / "9606.protein.links.v12.0"
+
+    df = pd.read_csv(path, sep="\t", header=0)
+    return df
 
 if __name__ == "__main__":
     df = load_info()
