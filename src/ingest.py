5ea6414da7370d9bb6c4707ec0f70f17535da1fa
anjannis <an.jannis@icloud.com>
2026-09-23 16:16:27 +0200

    restructured ingest.py

--- src/ingest.py
+++ src/ingest.py
@@ -3,29 +3,33 @@ from pathlib import Path
 
 RAW_DIR = Path(__file__).parent.parent / "data" / "raw"
 
-print(RAW_DIR)
-
-
-def load_info() -> pd.DataFrame:
+def load_info(raw_dir: Path = RAW_DIR) -> pd.DataFrame:
     """
     Load the STRING protein info file into a DataFrame.
     """
-    path = RAW_DIR / "9606.protein.info.v12.0.txt"
+    path = raw_dir / "9606.protein.info.v12.0.txt"
 
-    df = pd.read_csv(path, sep="\t", header=0)
+    df = pd.read_csv(path, sep="\t")
+    df = df.rename(columns={"#string_protein_id": "string_protein_id"})
     return df
 
-def load_links() -> pd.DataFrame:
+def load_links(raw_dir: Path = RAW_DIR) -> pd.DataFrame:
     """
     Load the STRING protein link file into a Dataframe.
     """
-    path = RAW_DIR / "9606.protein.links.v12.0"
+    path = raw_dir / "9606.protein.links.v12.0.txt"
 
-    df = pd.read_csv(path, sep="\t", header=0)
+    df = pd.read_csv(path, sep=" ")
     return df
 
 if __name__ == "__main__":
-    df = load_info()
-    print(df.head())
-    print(df.shape)
-    print(df.dtypes)
+    RAW_DIR = Path(__file__).parent.parent / "data" / "raw"
+    df_info = load_info(RAW_DIR)
+    print(df_info.head())
+    print(df_info.shape)
+    print(df_info.dtypes)
+
+    df_links = load_links(RAW_DIR)
+    print(df_links.head())
+    print(df_links.shape)
+    print(df_links.dtypes)
