83f653fac21d6839ad2c2a58e7781dead982cf4a
anjannis <an.jannis@icloud.com>
2026-09-25 16:59:48 +0200

    added dtype to read_csv and in expploration notebook the interaction occruence count

--- src/ingest.py
+++ src/ingest.py
@@ -19,17 +19,16 @@ def load_links(raw_dir: Path = RAW_DIR) -> pd.DataFrame:
     """
     path = raw_dir / "9606.protein.links.v12.0.txt"
 
-    df = pd.read_csv(path, sep=" ")
+    df = pd.read_csv(path, sep=" ", dtype={"combined_score": "int16"})
     return df
 
 if __name__ == "__main__":
-    RAW_DIR = Path(__file__).parent.parent / "data" / "raw"
-    df_info = load_info(RAW_DIR)
+    df_info = load_info()
     print(df_info.head())
     print(df_info.shape)
     print(df_info.dtypes)
 
-    df_links = load_links(RAW_DIR)
+    df_links = load_links()
     print(df_links.head())
     print(df_links.shape)
     print(df_links.dtypes)
